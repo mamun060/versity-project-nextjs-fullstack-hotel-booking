67157df741a081fa17bf9579272776eac874959b
mamun060 <almamun.raju520@gmail.com>
2024-06-29 13:01:04 +0600

    search component searchParams and query perameter done

--- src/app/(home)/hotels/page.js
+++ src/app/(home)/hotels/page.js
@@ -3,12 +3,17 @@ import Filter from "@/components/search/Filter";
 import Search from "@/components/search/Search";
 
 
-const HotelListPage = () => {
+const HotelListPage = ({searchParams: {destination, checkin, checkout}}) => {
     return (
         <>
             <section className="bg-[url('/hero-bg.jpg')] bg-cover bg-no-repeat bg-center pt-[100px] pb-[60px]">
                 <div className="container items-center py-12 ">
-                    <Search fromList={true} />
+                    <Search 
+                        fromList={true}
+                        destination={destination}
+                        checkin={checkin}
+                        checkout={checkout}
+                     />
                 </div>
             </section>
             <section className="py-12">
