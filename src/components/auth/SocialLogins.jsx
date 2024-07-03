b1ba1a1c2013a2589ad772d0863cf0a64ca4ad9e
mamun060 <almamun.raju520@gmail.com>
2024-07-03 12:40:12 +0600

    Login page minor changes

--- src/components/auth/SocialLogins.jsx
+++ src/components/auth/SocialLogins.jsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import { signIn } from "next-auth/react";
 import Link from "next/link";
-// import { signIn } from "../../../auth";
 
 const SocialLogins = ({mode}) => {
   const handleAuth = (event) =>{
@@ -20,10 +19,10 @@ const SocialLogins = ({mode}) => {
          or Signup with
       </div>
       <div className="flex gap-4">
-        <button className=" w-full mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center">
+        {/* <button className=" w-full mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center">
           <Image src="/fb.png" alt="facebook" width={40} height={40} />
           <span>Facebook</span>
-        </button>
+        </button> */}
         <button
          onClick={handleAuth}
          className=" w-full mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center">
