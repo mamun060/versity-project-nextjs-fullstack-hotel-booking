ef619211677386186fc0641d19be4d459040ea90
Md Mamun <mamun@smartfieldservice.com>
2024-09-05 13:38:32 +0600

    build bug fixed in blog and blog details page

--- src/app/(home)/blog/[id]/page.js
+++ src/app/(home)/blog/[id]/page.js
@@ -5,49 +5,49 @@ const blogPosts = [
       id: 1,
       title: 'The Future Destion',
       description: ' is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Exploring the latest trends and technologies in web development.',
-      imageUrl: 'blog/one.jpg',
+      imageUrl: '/blog/one.jpg',
       date: 'August 24, 2024',
     },
     {
       id: 2,
       title: 'Honeymoon at Coxs Bazar',
       description: ' is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. A comprehensive guide to getting started with Tailwind CSS.',
-      imageUrl: 'blog/two.jpg',
+      imageUrl: '/blog/two.jpg',
       date: 'July 18, 2024',
     },
     {
       id: 3,
       title: 'Honeymoon at Franch',
       description: ' is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Boost the performance of your Next.js applications with these tips.',
-      imageUrl: 'blog/three.jpg',
+      imageUrl: '/blog/three.jpg',
       date: 'June 10, 2024',
     },
     {
       id: 4,
       title: 'Honeymoon at Italy',
       description: ' is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Exploring the latest trends and technologies in web development.',
-      imageUrl: 'blog/four.jpg',
+      imageUrl: '/blog/four.jpg',
       date: 'August 24, 2024',
     },
     {
       id: 5,
       title: 'Honeymoon at Japan',
       description: ' is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. A comprehensive guide to getting started with Tailwind CSS.',
-      imageUrl: 'blog/five.jpg',
+      imageUrl: '/blog/five.jpg',
       date: 'July 18, 2024',
     },
     {
       id: 6,
       title: 'Honeymoon at Germany',
       description: 'Boost the performance of your Next.js applications with these tips.',
-      imageUrl: 'blog/six.jpg',
+      imageUrl: '/blog/six.jpg',
       date: 'June 10, 2024',
     },
     {
       id: 7,
       title: 'Honeymoon at Thailand',
       description: ' is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Boost the performance of your Next.js applications with these tips.',
-      imageUrl: 'blog/seven.jpg',
+      imageUrl: '/blog/seven.jpg',
       date: 'June 10, 2024',
     },
 ];
@@ -65,7 +65,7 @@ export default function BlogDetails({params}) {
   return (
         <div className="bg-gray-100 min-h-screen flex items-center justify-center">
         <div className="max-w-4xl w-full mx-4 lg:mx-0 py-12 px-4 bg-white shadow-lg rounded-lg overflow-hidden">
-        <Image src={`../${blogPost.imageUrl}`} alt={blogPost.title || 'Decorative image'} width={640} height={256} className="w-full h-64 object-cover" />
+        <Image src={blogPost.imageUrl} alt={blogPost.title || 'Decorative image'} width={640} height={256} className="w-full h-64 object-cover" />
         <div className="p-8">
             <p className="text-gray-500 mb-8 text-center">{blogPost.date}</p>
             <h1 className="text-4xl font-bold mb-4 text-center">{blogPost.title}</h1>
